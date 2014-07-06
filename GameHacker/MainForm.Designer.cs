cf0b396a3f16982c2adc90f6fd41811a454032d1
cyendra <g1483335912@gmail.com>
2014-07-06 13:23:54 +0800

    new

---
+++ GameHacker/MainForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace GameHacker
+{
+    partial class form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processList = new System.Windows.Forms.ListBox();
+            this.findBtn = new System.Windows.Forms.Button();
+            this.changeBtn = new System.Windows.Forms.Button();
+            this.talkLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numberBox = new System.Windows.Forms.TextBox();
+            this.changeBox = new System.Windows.Forms.TextBox();
+            this.refreshBtn = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // processList
+            // 
+            this.processList.FormattingEnabled = true;
+            this.processList.ItemHeight = 12;
+            this.processList.Location = new System.Drawing.Point(12, 12);
+            this.processList.Name = "processList";
+            this.processList.Size = new System.Drawing.Size(255, 196);
+            this.processList.TabIndex = 0;
+            this.processList.SelectedIndexChanged += new System.EventHandler(this.processList_SelectedIndexChanged);
+            // 
+            // findBtn
+            // 
+            this.findBtn.Location = new System.Drawing.Point(499, 54);
+            this.findBtn.Name = "findBtn";
+            this.findBtn.Size = new System.Drawing.Size(83, 23);
+            this.findBtn.TabIndex = 1;
+            this.findBtn.Text = "寻找";
+            this.findBtn.UseVisualStyleBackColor = true;
+            this.findBtn.Click += new System.EventHandler(this.findBtn_Click);
+            // 
+            // changeBtn
+            // 
+            this.changeBtn.Location = new System.Drawing.Point(499, 214);
+            this.changeBtn.Name = "changeBtn";
+            this.changeBtn.Size = new System.Drawing.Size(83, 23);
+            this.changeBtn.TabIndex = 2;
+            this.changeBtn.Text = "修改";
+            this.changeBtn.UseVisualStyleBackColor = true;
+            this.changeBtn.Click += new System.EventHandler(this.changeBtn_Click);
+            // 
+            // talkLabel
+            // 
+            this.talkLabel.AutoSize = true;
+            this.talkLabel.Location = new System.Drawing.Point(273, 12);
+            this.talkLabel.Name = "talkLabel";
+            this.talkLabel.Size = new System.Drawing.Size(125, 12);
+            this.talkLabel.TabIndex = 3;
+            this.talkLabel.Text = "请先选择要修改的游戏";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "搜索结果";
+            // 
+            // numberBox
+            // 
+            this.numberBox.Location = new System.Drawing.Point(275, 27);
+            this.numberBox.Name = "numberBox";
+            this.numberBox.Size = new System.Drawing.Size(307, 21);
+            this.numberBox.TabIndex = 5;
+            // 
+            // changeBox
+            // 
+            this.changeBox.Location = new System.Drawing.Point(275, 187);
+            this.changeBox.Name = "changeBox";
+            this.changeBox.Size = new System.Drawing.Size(307, 21);
+            this.changeBox.TabIndex = 6;
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Location = new System.Drawing.Point(192, 214);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(75, 23);
+            this.refreshBtn.TabIndex = 7;
+            this.refreshBtn.Text = "刷新";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.FormattingEnabled = true;
+            this.resultBox.ItemHeight = 12;
+            this.resultBox.Location = new System.Drawing.Point(273, 81);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(309, 88);
+            this.resultBox.TabIndex = 8;
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 243);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.refreshBtn);
+            this.Controls.Add(this.changeBox);
+            this.Controls.Add(this.numberBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.talkLabel);
+            this.Controls.Add(this.changeBtn);
+            this.Controls.Add(this.findBtn);
+            this.Controls.Add(this.processList);
+            this.Name = "form";
+            this.Text = "Game Hacker";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox processList;
+        private System.Windows.Forms.Button findBtn;
+        private System.Windows.Forms.Button changeBtn;
+        private System.Windows.Forms.Label talkLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox numberBox;
+        private System.Windows.Forms.TextBox changeBox;
+        private System.Windows.Forms.Button refreshBtn;
+        private System.Windows.Forms.ListBox resultBox;
+    }
+}
+
---
+++ GameHacker/MainForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace GameHacker
+{
+
+    public partial class form : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+        private List<Process> WindowedProcesses = new List<Process>();
+        private List<IntPtr> AddrList = new List<IntPtr>();
+        private bool isFirstSearch = true;
+        private Process SelectedProcess;
+
+        private void RefreshProcessList()
+        {
+            SelectedProcess = null;
+            processList.Items.Clear();
+            WindowedProcesses.Clear();
+            changeBox.Enabled = false;
+            talkLabel.Text = "请先选择要修改的游戏";
+            foreach (var p in Process.GetProcesses())
+            {
+                if (p.MainWindowHandle != IntPtr.Zero)
+                {
+                    if (!string.IsNullOrEmpty(p.MainWindowTitle))
+                    {
+                        processList.Items.Add(p.MainWindowTitle);
+                        WindowedProcesses.Add(p);
+                    }
+                }
+            }
+        }
+
+        public form()
+        {
+            InitializeComponent();
+        }
+
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            RefreshProcessList();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            RefreshProcessList();
+        }
+
+        private List<IntPtr> CreateAddrList(IntPtr baseAddr, int value)
+        {
+            int bytesRead;
+            byte[] buffer = new byte[4096];
+            bool ok;
+            List<IntPtr> result = new List<IntPtr>();
+            ok = ReadProcessMemory(SelectedProcess.Handle, baseAddr, buffer, 4096, out bytesRead);
+            if (!ok) return null;
+            int currentVal;
+            for (int i = 0; i < 4096 - 3;i++)
+            {
+                currentVal = BitConverter.ToInt32(buffer, i);
+                if (currentVal == value)
+                {
+                    IntPtr addr = new IntPtr(baseAddr.ToInt32() + i);
+                    result.Add(addr);
+                    i += 3;
+                }
+            }
+            return result;
+        }
+
+        private void RefreshAddrList(int value)
+        {
+            var la = AddrList.ToList();
+            AddrList.Clear();
+
+            byte[] buffer = new byte[4];
+            int bytesRead;
+            foreach (var i in la)
+            {
+                ReadProcessMemory(SelectedProcess.Handle, i, buffer, 4, out bytesRead);
+                if (BitConverter.ToInt32(buffer, 0) == value) AddrList.Add(i);
+            }
+        }
+
+        private void findBtn_Click(object sender, EventArgs e)
+        {
+            resultBox.Items.Clear();
+            if (SelectedProcess == null) return;
+            if (isFirstSearch)
+            {
+                uint baseAddr = 0x00010000;
+                uint endAddr = 0x7ffeffff;
+                for (uint i = baseAddr; i < endAddr; i += (4 * 1024))
+                {
+                    var addrs = CreateAddrList(new IntPtr(i), int.Parse(numberBox.Text));
+                    if (addrs != null) AddrList.AddRange(addrs);
+                }
+                isFirstSearch = false;
+            }
+            else
+            {
+                RefreshAddrList(int.Parse(numberBox.Text));
+            }
+            label2.Text = "找到 " + AddrList.Count.ToString() + " 个结果";
+            MessageBox.Show(label2.Text);
+            if (AddrList.Count == 1) changeBox.Enabled = true;
+            if (AddrList.Count < 1000)
+            {
+                foreach (var i in AddrList)
+                {
+                    resultBox.Items.Add(i.ToString());
+                }
+            }
+        }
+
+        private void changeBtn_Click(object sender, EventArgs e)
+        {
+            int value;
+            if (!int.TryParse(changeBox.Text, out value))
+            {
+                MessageBox.Show("溢出!");
+                return;
+            }
+            var buffer = BitConverter.GetBytes(value);
+            int bytesWritten;
+            WriteProcessMemory(SelectedProcess.Handle, AddrList[0], buffer, 4, out bytesWritten);
+        }
+
+        private void processList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idx = processList.SelectedIndex;
+            if (idx<0 || idx >= WindowedProcesses.Count)
+            {
+                RefreshProcessList();
+                return;
+            }
+            SelectedProcess = WindowedProcesses[idx];
+            talkLabel.Text = (string)processList.SelectedItem;
+        }
+    }
+}
